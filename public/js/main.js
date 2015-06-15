86c9e3e4cb1137cdbe5236f541cc401ffcf7883d
Aaron Bieber <deftly@gmail.com>
2015-06-15 16:45:42 -0600

    gauges

--- public/js/main.js
+++ public/js/main.js
@@ -155,3 +155,15 @@ $( 'document' ).ready( function() {
 	});
 
 });
+
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4f47bcdacb25bc795100000a');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
